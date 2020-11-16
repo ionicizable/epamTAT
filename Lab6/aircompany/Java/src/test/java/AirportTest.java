9732d40415836688e362b4d996cc26a6924a23a0
ilya <ilya.m.shapiro@gmail.com>
2020-11-16 18:37:21 +0300

    updated VTOL to full abbreviature in ExperimentalTypes class

--- Lab6/aircompany/Java/src/test/java/AirportTest.java
+++ Lab6/aircompany/Java/src/test/java/AirportTest.java
@@ -28,7 +28,7 @@ public class AirportTest {
             new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
             new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT),
             new ExperimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
-            new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
+            new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VERTICAL_TAKEOFF_AND_LANDING, ClassificationLevel.TOP_SECRET)
     );
 
     private static PassengerPlane planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
